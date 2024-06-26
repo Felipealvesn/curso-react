7200b72a33beaca56907e1ca17678bfb107fcc80
Felipealvesn <felposoalves@gmail.com>
2024-06-26 17:44:07 -0300

    condicionais react...

--- my-project/src/components/Condicionais.js
+++ my-project/src/components/Condicionais.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import styles from './Forms.module.css';
 
 function Condicionais(){
 
@@ -6,16 +7,22 @@ function Condicionais(){
         e.preventDefault()
         setUser(Email)
     }
+    function limparEmail(){
+      setUser("")
+    }
     const [Email, setEmail] = useState()
     const [userEmail, setUser] = useState()
     return(
         <div>
             <h2>Cadastro de E-mail</h2>
-            <form>
-                <input type="email" required id="email" placeholder="Digite seu E-mail" className="input" onChange={(e) => setEmail(e.target.value)}/> 
-                <input type="submit"  value="Cadastrar" className="submit" onClick={cadastrado}/>
+            <form className={styles.form}>
+                <input type="email" required id="email" placeholder="Digite seu E-mail" className={styles.input} onChange={(e) => setEmail(e.target.value)}/> 
+                <input type="submit"  value="Cadastrar" className={styles.submit} onClick={cadastrado}/>
                 {userEmail &&(
+                    <div>
                     <p>E-mail: {userEmail}</p>
+                    <button onClick={limparEmail}>Limpar emali</button>
+                    </div>
                 )}
             </form>
         </div>
